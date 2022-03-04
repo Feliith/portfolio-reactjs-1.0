44a63cc7fdc4ea43cd4afa9beda7675e438e7fcb
unknown <felipelmrobles03@gmail.com>
2022-03-04 12:44:21 -0300

    initial commit + navbar addon

---
+++ src/components/Contacts.js
@@ -0,0 +1,29 @@
+export const Social = [
+    {
+        title: 'GitHub',
+        icon: 'fa fa-github',
+        url: 'https://github.com/Feliith',
+        class: 'nav-contact'
+    },
+    {
+        title: 'LinkedIn',
+        icon: 'fa fa-linkedin',
+        url: 'https://www.linkedin.com/in/felipe-robles-26708b212/',
+        class: 'nav-contact'
+    },
+    {
+        title: 'Instagram',
+        icon: 'fa fa-instagram',
+        url: 'https://www.instagram.com/feliith/',
+        class: 'nav-contact'
+    }
+]
+
+export const Number =  [
+    {
+        title: '(35) 99858-1910',
+        icon: 'fa fa-whatsapp',
+        url: '',
+        class: 'nav-contact'
+    }
+]
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react'
+import './Navbar.css'
+import { Social , Number } from '../Contacts'
+
+class Navbar extends Component {
+    state = {clicked: false}
+
+    handleClick = () => {
+        this.setState({clicked: !this.state.clicked})
+    }
+
+    render() {
+        return (
+            <>
+                <nav>
+                    <div className="nav-logo">
+                        <a href="/">Fe</a>
+                    </div>
+                    <div className="nav-contacts">
+                        {Social.map((item, index) => {
+                            return (
+                                <a href={item.url} target="_blank" className={item.class}>
+                                    <i className={item.icon}></i>
+                                </a>
+                            )
+                        })}
+                    </div>
+                    <div className="nav-number">
+                        {Number.map((item, index) => {
+                            return (
+                                <>
+                                    <a href={item.item} target="_blank" className={item.class}>
+                                        <i className={item.icon}></i>
+                                    </a>
+                                    <h2>{item.title}</h2>
+                                </>
+                            )
+                        })}
+                    </div>
+                    <div className="nav-burger" onClick={this.handleClick}>
+                        <i className='fa fa-bars'/>
+                    </div>
+                </nav>
+                <div className={this.state.clicked ? 'sidebar' : 'sidebar hide'}>
+                    <div className="side-close" onClick={this.handleClick}>
+                        <i className='fa fa-times'/>
+                    </div>
+                </div>
+            </>
+        )
+    }
+}
+
+export default Navbar
